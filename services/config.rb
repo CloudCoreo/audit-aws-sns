0dbc35975b3c3e9de5bbe873736fd56bb758aa78
George Moberly <george.moberly@gmail.com>
2017-02-18 05:46:45 -0800

    test inventory

--- services/config.rb
+++ services/config.rb
@@ -5,27 +5,27 @@ coreo_uni_util_variables "sns-vars" do
             ])
 end
 
-# coreo_aws_rule "sns-inventory" do
-#   action :define
-#   service :sns
-#   link "http://kb.cloudcoreo.com/mydoc-inventory.html"
-#   include_violations_in_count false
-#   display_name "KMS Inventory"
-#   description "This rule performs an inventory on all sns objects in the target AWS account."
-#   category "Inventory"
-#   suggested_action "None."
-#   level "Informational"
-#   meta_cis_id "99.999"
-#   objectives ["TBS"]
-#   audit_objects ["object.TBS"]
-#   operators ["=~"]
-#   raise_when [//]
-#   id_map "object.TBS"
-# end
+coreo_aws_rule "sns-inventory" do
+  action :define
+  service :sns
+  link "http://kb.cloudcoreo.com/mydoc-inventory.html"
+  include_violations_in_count false
+  display_name "KMS Inventory"
+  description "This rule performs an inventory on all sns objects in the target AWS account."
+  category "Inventory"
+  suggested_action "None."
+  level "Informational"
+  meta_cis_id "99.999"
+  objectives ["topics"]
+  audit_objects ["object.topics"]
+  operators ["=~"]
+  raise_when [//]
+  id_map "object.topics"
+end
 
-# coreo_aws_rule_runner "advise-sns" do
-#   rules ${AUDIT_AWS_SNS_ALERT_LIST}
-#   action :run
-#   service :sns
-#   regions ${AUDIT_AWS_SNS_REGIONS}
-# end
+coreo_aws_rule_runner "advise-sns" do
+  rules ${AUDIT_AWS_SNS_ALERT_LIST}
+  action :run
+  service :sns
+  regions ${AUDIT_AWS_SNS_REGIONS}
+end
