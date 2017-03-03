0bc9c175c3da70aacda1bb84baa64f842c052248
colemai <colemai@tcd.ie>
2017-03-03 11:38:57 -0800

    CON-399 added subs inv

--- services/config.rb
+++ services/config.rb
@@ -1,20 +1,35 @@
-
-coreo_aws_rule "sns-inventory" do
+coreo_aws_rule "sns-topics-inventory" do
   action :define
   service :sns
   link "http://kb.cloudcoreo.com/mydoc-inventory.html"
   include_violations_in_count false
-  display_name "SNS Inventory"
-  description "This rule performs an inventory on all sns objects in the target AWS account."
+  display_name "SNS Topics Inventory"
+  description "This rule performs an inventory on all sns topics in the target AWS account."
   category "Inventory"
   suggested_action "None."
   level "Informational"
-  meta_cis_id "99.999"
   objectives ["topics"]
   audit_objects ["object.topics.topic_arn"]
   operators ["=~"]
   raise_when [//]
   id_map "object.topics.topic_arn"
+end
+
+coreo_aws_rule "sns-subscriptions-inventory" do
+  action :define
+  service :sns
+  link "http://kb.cloudcoreo.com/mydoc-inventory.html"
+  include_violations_in_count false
+  display_name "SNS Subscriptions Inventory"
+  description "This rule performs an inventory on all sns subscriptions in the target AWS account."
+  category "Inventory"
+  suggested_action "None."
+  level "Informational"
+  objectives ["subscriptions"]
+  audit_objects ["object.subscriptions.subscription_arn"]
+  operators ["=~"]
+  raise_when [//]
+  id_map "object.subscriptions.subscription_arn"
 end
 
 coreo_uni_util_variables "sns-planwide" do
