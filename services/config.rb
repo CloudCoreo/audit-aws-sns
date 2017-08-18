232331829c3638080a34c81e0a147d5ee98d97d1
omurbek.jk@gmail.com <omurbek.jk@gmail.com>
2017-08-18 19:19:45 +0600

    PLA-4508 changed s3-bucket-policy-service name

--- services/config.rb
+++ services/config.rb
@@ -301,7 +301,7 @@ coreo_aws_s3_policy "cloudcoreo-audit-aws-sns-policy" do
   EOF
 end
 
-coreo_aws_s3_bucket "cloudcoreo-audit-aws-sns" do
+coreo_aws_s3_bucket "${AUDIT_AWS_SNS_S3_NOTIFICATION_BUCKET_NAME}" do
   action((("${AUDIT_AWS_SNS_S3_NOTIFICATION_BUCKET_NAME}".length > 0) ) ? :create : :nothing)
   bucket_policies ["cloudcoreo-audit-aws-sns-policy"]
   region "us-east-1"
